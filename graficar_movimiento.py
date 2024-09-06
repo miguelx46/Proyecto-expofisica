cda65d12f94ead4150773d50a3fad5b7ff6e1658
miguelx46 <139667111+miguelx46@users.noreply.github.com>
2024-09-06 11:03:09 -0500

    Add files via upload

--- graficar_movimiento.py
+++ graficar_movimiento.py
@@ -8,16 +8,23 @@ ejex = np.linspace(0, 50, limx)
 ejey = np.linspace(0, 50, limy)
 print("Digite el ángulo de inclinación")
 theta = float(input())
+print("Digite la velocidad inicial")
+vo = float(input()) 
 g = -9.81
-#tomando valores de x, y para guardarlos en 2 vectores
+# tomando valores de x, y para guardarlos en 2 vectores
 xarray = np.array([limx])
 yarray = np.array([limy])
 
 for t in range(0, limx):
- y = np.sin(theta)*t + (g*t**2)/2
- x = np.cos(theta)*t   
+ y = np.sin(theta)*vo*t + (g*t**2)/2
+ x = np.cos(theta)*vo*t   
  xarray [t] = x
  yarray [t] = y
 
+print("valores de y")
+for t in range(0, limy):
+ print(yarray[t])
+
+print("valores de x")
 for t in range(0, limx):
- print(x, "," +y)
+ print(xarray[t]) 
